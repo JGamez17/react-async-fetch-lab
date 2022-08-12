e3469a196b2e28088a238219269a11723a6254b3
JGamez17 <github email address>
2022-08-11 22:30:30 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
-// create your App component here
+// create your App component that returns a list of people currently in space.
+
+import React, { Component } from "react";
+
+class App extends React.Component {
+
+    state = {
+        peopleInSpace: [],
+    };
+
+
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+        // this.setState({
+        //     peopleInSpace: data.people,
+    }
+
+    render() {
+        return (
+
+            <div>
+                <h1>  "peopleInSpace"   </h1>
+
+            </div >
+        )
+    }
+}
+
+export default App; 
